ea25fb2e55d52090b58898e5bba7b2be22a66ad9
nikhilempower <nikhilempower@gmail.com>
2025-03-20 13:37:41 +0530

    Custom Settings

--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@ This is a video application on backend with javascript
 
 # Dependency
 npm i mongoose dotenv express
+npm i cookie-parser cors
 
 # Dev Dependency
  npm i -D nodemon 
--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MongoDB Connection error", error);
+        console.log("MongoDB Connection Failed...! ", error);
         process.exit(1);
     }
 }
